e6aec3f3ccb465c800643b0f8d20a2e00cb222b9
stan-works <stanworks.official@gmail.com>
2026-04-30 11:28:40 +0900

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -6,7 +6,7 @@ window.seminarEvents = [
     //     id: 12, title: "障がいのある子の親なきあと問題と自立に向けた『将来設計勉強会』", area: "大阪",
     //     location: "大阪府寝屋川市奏町41-1 寝屋川市立市民会館", date: ["2026-04-16", "2026-04-25"], time: ["10:00", "13:30"], endTime: "約2時間半",
     //     speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-    //     image: "../images/seminar008.jpeg", content: "4.16は午前のみ", formUrl: "https://ssl.form-mailer.jp/fms/5994dede876617"
+    //     image: "images/seminar008.jpeg", content: "4.16は午前のみ", formUrl: "https://ssl.form-mailer.jp/fms/5994dede876617"
     // },
 
    {
@@ -24,60 +24,60 @@ window.seminarEvents = [
         id: 10, title: "障がいのある子の親なきあと問題と自立に向けた『将来設計勉強会』", area: "大阪",
         location: "大阪府寝屋川市奏町41-1 寝屋川市立市民会館", date: ["2026-04-16", "2026-04-25"], time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar007.jpeg", content: "4/16は午前のみ", formUrl: "https://ssl.form-mailer.jp/fms/5994dede876617"
+        image: "images/seminar007.jpeg", content: "4/16は午前のみ", formUrl: "https://ssl.form-mailer.jp/fms/5994dede876617"
     },
     {
         id: 9, title: "障がいのある子の親なきあとの問題と自立に向けた将来設計勉強会", area: "兵庫",
         location: "兵庫県姫路市南駅前町123 姫路じばさんびる", date: ["2026-03-11", "2026-03-14"], time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar006.jpeg", content: "3/11は午前のみ", formUrl: ""
+        image: "images/seminar006.jpeg", content: "3/11は午前のみ", formUrl: ""
     },
     {
         id: 8, title: "障がいのある子の親なきあとの問題と自立に向けた将来設計の勉強会", area: "東京",
         location: "東京都品川区東大井5-18-1 きゅりあん", date: ["2026-03-01", "2026-03-04"], time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "一般社団法人あしたパートナーズ代表理事 株式会社あしたパートナーズ 代表取締役 首藤 徹也", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar005.jpeg", content: "", formUrl: ""
+        image: "images/seminar005.jpeg", content: "", formUrl: ""
     },
     {
         id: 7, title: "障がいのある子の親なきあとの問題と自立に向けた将来設計の勉強会", area: "愛知",
         location: "愛知県名古屋市中村区名駅4丁目4-38 ウインクあいち", date: ["2026-02-25", "2026-02-28"], time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "一般社団法人あしたパートナーズ代表理事 株式会社あしたパートナーズ 代表取締役 首藤 徹也", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar005.jpeg", content: "", formUrl: ""
+        image: "images/seminar005.jpeg", content: "", formUrl: ""
     },
     {
         id: 6, title: "障がいのある子の「親なきあと問題」と自立に向けた将来設計勉強会", area: "大阪",
         location: "三田市天神1丁目5-33 三田市商工会館5階多目的ホール", date: ["2026-01-28", "2026-02-11"], time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar004.png", content: "1/28は午前中のみ", formUrl: ""
+        image: "images/seminar004.png", content: "1/28は午前中のみ", formUrl: ""
     },
     {
         id: 5, title: "発達障がいのある子の親なきあとの問題と自立に向けた将来設計勉強会", area: "大阪",
         location: "大阪市中央区北浜東3-14 大阪府立労働センター（エル・おおさか）", date: "2025-11-03", time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar003.jpeg", content: "", formUrl: ""
+        image: "images/seminar003.jpeg", content: "", formUrl: ""
     },
     {
         id: 4, title: "発達障がいのある子の親なきあとの問題と自立に向けた将来設計勉強会", area: "大阪",
         location: "大阪市中央区本町1-4-5 大阪産業創造館", date: "2025-10-19", time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar003.jpeg", content: "", formUrl: ""
+        image: "images/seminar003.jpeg", content: "", formUrl: ""
     },
     {
         id: 3, title: "「親なき後の問題と自立に向けた将来設計」勉強会", area: "名古屋",
         location: "名古屋市中区錦1-18-22 名古屋サンスカイルーム", date: "2025-10-26", time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "一般社団法人 あしたパートナーズ 代表 首藤 徹也", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar002.jpg", content: "", formUrl: ""
+        image: "images/seminar002.jpg", content: "", formUrl: ""
     },
     {
         id: 2, title: "「親なき後の問題と自立に向けた将来設計」勉強会", area: "岐阜",
         location: "岐阜県岐阜市橋本町1-10 じゅうろくプラザ", date: "2025-10-11", time: ["10:00", "13:30"], endTime: "約2時間半",
         speaker: "一般社団法人 あしたパートナーズ 代表 首藤 徹也", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-        image: "../images/seminar002.jpg", content: "", formUrl: ""
+        image: "images/seminar002.jpg", content: "", formUrl: ""
     },
     {
         id: 1, title: "親なき後の自立を目指した生活設計とお金の勉強会", area: "兵庫",
         location: "神戸市中央区御幸通8-1-6 神戸国際会館セミナーハウス", date: "2025-09-20", time: "10:00", endTime: "約2時間半",
         speaker: "", organizer: "",
-        image: "../images/seminar001.jpeg", content: "", formUrl: ""
+        image: "images/seminar001.jpeg", content: "", formUrl: ""
     }
 ];
