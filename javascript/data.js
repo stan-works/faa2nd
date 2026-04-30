6f34ac27347753b21167b1da529aabc3178fc5b6
stan-works <stanworks.official@gmail.com>
2026-04-30 10:49:06 +0900

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -6,7 +6,7 @@ window.seminarEvents = [
     //     id: 12, title: "障がいのある子の親なきあと問題と自立に向けた『将来設計勉強会』", area: "大阪",
     //     location: "大阪府寝屋川市奏町41-1 寝屋川市立市民会館", date: ["2026-04-16", "2026-04-25"], time: ["10:00", "13:30"], endTime: "約2時間半",
     //     speaker: "株式会社 整理収納教育士 代表 魚林 佐起子", organizer: "一般社団法人 ファイナンシャルアドバイザー協会",
-    //     image: "../images/seminar008.jpeg", content: "4.16は午前のみ", formUrl: "https://ssl.form-mailer.jp/fms/5994dede876617"
+    //     image: "/images/seminar008.jpeg", content: "4.16は午前のみ", formUrl: "https://ssl.form-mailer.jp/fms/5994dede876617"
     // },
 
    {
